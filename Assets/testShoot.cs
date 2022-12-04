d471cffa453d9375d1352e17939e26d349993611
Your kaminosekai54 <culpin.alexis@laposte.net>
2022-12-04 16:50:28 +0100

    adding ammo count system

--- Assets/testShoot.cs
+++ Assets/testShoot.cs
@@ -8,23 +8,51 @@ public class testShoot : MonoBehaviour
     public SimpleShoot simpleShoot;
     public float damage = 20f;
     public float impactForce = 20f;
+public AudioSource shootingSound;
+    public AudioSource noAmmoAudioSound;
+    public AudioSource reloadSound;
+    // for ammo count
+    public int ammoCount;
+    public int maxAmmo = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        shootingSound= GetComponent<AudioSource>();
+        noAmmoAudioSound = GetComponent<AudioSource>();
+        reloadSound= GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown("r"))
+        {
+reload();
+        }
         if (Input.GetKeyDown("f"))
         {
+            if (ammoCount > 0 )
+            {
             Shoot();
             simpleShoot.StartShoot();
             GetComponent<AudioSource>().Play();
+            shootingSound.Play();
+            ammoCount-=1;
         }
-        
+        else 
+        {
+noAmmoAudioSound.Play();
+
+        }
+        }
     }
+
+void reload()
+{
+ammoCount = maxAmmo;
+reloadSound.play();
+}
 
     void Shoot()
     {
